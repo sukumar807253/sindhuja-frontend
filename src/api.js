c274476962479aecccbc6e632b3b36bc7510b523
sugu <sugukumar778@gmail.com>
2026-02-10 13:46:48 +0530

    Fix API baseURL for production

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://sindhuja-colloction1.onrender.com"
+  baseURL: "https://sindhuja-colloction1.onrender.com",
+  withCredentials: true
 });
 
 export default API;
